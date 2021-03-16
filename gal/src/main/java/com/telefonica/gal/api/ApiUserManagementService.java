97e2d5502ed32418634eddd9c3e3c7a313dd4dc7
Gatby Romero <gatby.romero@entelgydigital.com>
2021-03-16 19:23:29 +0100

    Ajuste UserManagementService

--- gal/src/main/java/com/telefonica/gal/api/ApiUserManagementService.java
+++ gal/src/main/java/com/telefonica/gal/api/ApiUserManagementService.java
@@ -1,8 +1,9 @@
 package com.telefonica.gal.api;
 
 import com.telefonica.gal.wsdl.northbound.provManagement.CreateUser;
+import com.telefonica.gal.wsdl.northbound.provManagement.CreateUserResponse;
 import com.telefonica.gal.wsdl.northbound.provManagement.UserCreationType;
 
 public interface ApiUserManagementService {
-    CreateUser callWsUserManagementCreateUser (UserCreationType userCreationType);
+    CreateUserResponse callWsUserManagementCreateUser (UserCreationType userCreationType);
 }
--- gal/src/main/java/com/telefonica/gal/api/ApiUserManagementServiceImpl.java
+++ gal/src/main/java/com/telefonica/gal/api/ApiUserManagementServiceImpl.java
@@ -1,7 +1,9 @@
 package com.telefonica.gal.api;
 
 import com.telefonica.gal.wsdl.northbound.provManagement.CreateUser;
+import com.telefonica.gal.wsdl.northbound.provManagement.CreateUserResponse;
 import com.telefonica.gal.wsdl.northbound.provManagement.UserCreationType;
+import com.telefonica.gal.wsdl.northbound.provManagement.UserIdType;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
@@ -13,10 +15,13 @@ public class ApiUserManagementServiceImpl implements ApiUserManagementService {
     @PayloadRoot(namespace = "http://www.telefonica.com/wsdl/UNICA/SOAP/OProv_Management/v7/local",
             localPart = "createUser")
     @ResponsePayload
-    public CreateUser callWsUserManagementCreateUser(@RequestPayload UserCreationType userCreationType) {
+    public CreateUserResponse callWsUserManagementCreateUser(@RequestPayload UserCreationType userCreationType) {
 
-        CreateUser response = new CreateUser();
-        response.setUserCreation(userCreationType);
+        UserIdType userIdType = new UserIdType();
+        userIdType.setAlias("ProvManagement");
+
+        CreateUserResponse response = new CreateUserResponse();
+        response.setGlobalUserID(userIdType);
 
         return response;
     }
