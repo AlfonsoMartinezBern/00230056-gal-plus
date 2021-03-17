da1fa3226632ed0666b4e85f82d87609519ba77e
Javier Sánchez González <javier.sanchez@entelgy.com>
2021-03-17 17:10:08 +0100

    Dynamic Routing adaptado a los JSON nuevos

--- dynamicRouting/src/main/java/com/example/demo/apidemo/ConfigService.java
+++ dynamicRouting/src/main/java/com/example/demo/apidemo/ConfigService.java
@@ -4,25 +4,42 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 import org.json.JSONObject;
 
 public class ConfigService {
-	
+
 	static JSONObject conf;
-	
+
 	@SuppressWarnings("resource")
 	public static JSONObject chargeConf() throws IOException {
-		BufferedReader reader = new BufferedReader(new FileReader(new File("C:/Develop/STSWorkspace/demo/directory/conf.json")));
+		BufferedReader reader = new BufferedReader(
+				new FileReader(new File("C:/Develop/Repositorio/00230056-gal-plus/dynamicRouting/src/main/resources/mapConfig/DynamicRoutingTD.json")));
 		String linea, json = "";
-		while ((linea = reader.readLine()) != null ) {
+		while ((linea = reader.readLine()) != null) {
 			json = json + linea;
 		}
-//		System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++");
-//		System.out.println("Servicio: " + conf.getJSONObject("example1").get("servicio"));
-//		System.out.println("EndPoint: " + conf.getJSONObject("example1").get("endpoint"));
-//		System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++");
-		
 		return new JSONObject(json);
+	}
+	
+	@SuppressWarnings("resource")
+	public static ArrayList<JSONObject> chargeConfArray() throws IOException {
+		ArrayList<JSONObject> jsons = new ArrayList<>();
+		BufferedReader reader = new BufferedReader(
+				new FileReader(new File("C:/Develop/Repositorio/00230056-gal-plus/dynamicRouting/src/main/resources/mapConfig/DynamicRoutingTD.json")));
+		String linea, json = "";
+		while ((linea = reader.readLine()) != null) {
+			json = json + linea;
+		}
+		Iterator<Object> jsonarray = new JSONObject(json).getJSONArray("dynamicRoutingTD").iterator();
+		
+		while (jsonarray.hasNext()) {
+//			System.out.println(jsonarray.next());
+			jsons.add((JSONObject) jsonarray.next());
+		}
+		
+		return jsons;
 	}
 }
