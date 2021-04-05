f36c45bb9fbd71be6a2c539a6d047509e50c1759
Gatby Romero <gatby.romero@entelgydigital.com>
2021-04-05 11:16:24 +0200

    Handler interceptor del mensaje

--- gal/src/main/java/com/telefonica/gal/api/ApiUserManagementServiceImpl.java
+++ gal/src/main/java/com/telefonica/gal/api/ApiUserManagementServiceImpl.java
@@ -1,42 +1,47 @@
 package com.telefonica.gal.api;
 
+import com.telefonica.gal.configuration.ConfigurationHandler;
 import com.telefonica.gal.service.userManagement.UserManagementService;
 import com.telefonica.gal.wsdl.northbound.provManagement.CreateUser;
 import com.telefonica.gal.wsdl.southbound.gvp.CreateUserResponse;
-import org.springframework.web.context.support.XmlWebApplicationContext;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Enumeration;
+import javax.xml.bind.JAXBElement;
+import javax.xml.ws.handler.MessageContext;
+import javax.xml.ws.handler.soap.SOAPMessageContext;
 
 @Endpoint
 public class ApiUserManagementServiceImpl implements ApiUserManagementService {
+
     public static final String NAMESPACE = "http://www.telefonica.com/wsdl/UNICA/SOAP/OProv_Management/v7/local";
 
     private final HttpServletRequest httpServletRequest;
 
     private final UserManagementService userManagementService;
 
-    public ApiUserManagementServiceImpl(UserManagementService userManagementService, HttpServletRequest httpServletRequest) {
+    private final ConfigurationHandler configurationHandler;
+
+    public ApiUserManagementServiceImpl(UserManagementService userManagementService, HttpServletRequest httpServletRequest,
+                                        ConfigurationHandler configurationHandler) {
         this.userManagementService = userManagementService;
         this.httpServletRequest = httpServletRequest;
+        this.configurationHandler = configurationHandler;
+
     }
 
     @PayloadRoot(namespace = NAMESPACE, localPart = "createUser")
     @ResponsePayload
-    public CreateUserResponse callWsUserManagementCreateUser(@RequestPayload CreateUser createUserRequest) {
-
-        Enumeration<String> headers = httpServletRequest.getHeaderNames();
-
-        while (headers.hasMoreElements()) {
-            System.out.println("Header Gal: " + httpServletRequest.getHeader(headers.nextElement()));
-        }
+    public CreateUserResponse callWsUserManagementCreateUser(@RequestPayload CreateUser createUserRequest, MessageContext messageContext) {
+        SOAPMessageContext soapMessageContext = (SOAPMessageContext) messageContext;
+        configurationHandler.wsAddressInjectHandler().handleMessage(soapMessageContext);
 
         String serviceId = httpServletRequest.getHeader("wsa05");
-        //String wsFrom = httpServletRequest.getHeader("wsaFrom");
+
+
         CreateUserResponse response = new CreateUserResponse();
         response = userManagementService.callWsUserManagementCreateUser(createUserRequest, serviceId);
 
--- gal/src/main/java/com/telefonica/gal/configuration/WsProvManagementConfig.java
+++ gal/src/main/java/com/telefonica/gal/configuration/WsProvManagementConfig.java
@@ -8,14 +8,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.ws.config.annotation.EnableWs;
 import org.springframework.ws.config.annotation.WsConfigurerAdapter;
-import org.springframework.ws.server.EndpointInterceptor;
-import org.springframework.ws.soap.server.endpoint.interceptor.PayloadValidatingInterceptor;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
 import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
 import org.springframework.xml.xsd.XsdSchemaCollection;
 import org.springframework.xml.xsd.commons.CommonsXsdSchemaCollection;
-
-import java.util.List;
 
 @EnableWs
 @Configuration
