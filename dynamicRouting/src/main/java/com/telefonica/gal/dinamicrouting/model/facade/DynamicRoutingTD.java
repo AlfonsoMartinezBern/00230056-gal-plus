e808da2e52b05e108b83ead038ed9896089d299a
Javier Sánchez González <javier.sanchez@entelgy.com>
2021-04-10 11:25:15 +0200

    Arreglando fallos commit anterior

--- dynamicRouting/src/main/java/com/telefonica/gal/dinamicrouting/model/facade/DynamicRoutingTD.java
+++
@@ -1,9 +0,0 @@
-package com.telefonica.gal.dinamicrouting.model.facade;
-
-import com.telefonica.gal.dinamicrouting.dto.RoutingTDInfo;
-import com.telefonica.gal.dinamicrouting.dto.RoutingTDKey;
-
-public interface DynamicRoutingTD {
-
-	public RoutingTDInfo getRouting(RoutingTDKey key);
-}
