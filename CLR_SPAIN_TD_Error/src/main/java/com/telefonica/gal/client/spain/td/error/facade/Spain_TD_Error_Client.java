756fdaff8e1f15e9bd7e656268ca16b74773eb49
jose maria <josemaria.gonzalez@entelgy.com>
2021-06-03 19:48:01 +0200

    correccion cliente

--- CLR_SPAIN_TD_Error/src/main/java/com/telefonica/gal/client/spain/td/error/facade/Spain_TD_Error_Client.java
+++ CLR_SPAIN_TD_Error/src/main/java/com/telefonica/gal/client/spain/td/error/facade/Spain_TD_Error_Client.java
@@ -4,40 +4,30 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import com.telefonica.gal.client.spain.td.error.msg.ErrorInfo;
 import com.telefonica.gal.client.spain.td.error.msg.ErrorKey;
+import com.telefonica.gal.client.spain.td.error.msg.ErrorResponse;
 
 @Service
 public class Spain_TD_Error_Client implements ISpainTDError {
     @Value("${error.uri}")
     private String URI;
 
-    public ErrorInfo search(ErrorKey errorKey) {
+    public ErrorResponse search(ErrorKey errorKey) {
         String errorURL =
                 URI
                         + "/search?operationApi=" + errorKey.getOperationApi() +
-                        "/search?serviceApi=" + errorKey.getServiceApi() +
+                        "&serviceApi=" + errorKey.getServiceApi() +
                         "&errorCode=" + errorKey.getErrorCode() +
                         "&errorCodeInterface=" + errorKey.getErrorCodeInterface() +
                         "&errorCodeOperation=" + errorKey.getErrorCodeOperation();
 
         RestTemplate plantilla = new RestTemplate();
 
-        return plantilla.getForObject(errorURL, ErrorInfo.class);
+        return plantilla.getForObject(errorURL, ErrorResponse.class);
         
-//        return mockSearch(errorKey);
     }
     
     
-    
-    
-    private ErrorInfo mockSearch(ErrorKey errorKey) {
-    	
-    	return new ErrorInfo(errorKey.getErrorCode(),"Descripcion del error para: " +errorKey.getErrorCode()+
-    			" errorCodeOperation=" + errorKey.getErrorCodeOperation() +
-                " errorCodeInterface=" + errorKey.getErrorCodeInterface() +
-                " serviceApi=" + errorKey.getServiceApi() +
-                " operationApi=" + errorKey.getOperationApi());
-    }
+
 
 }
