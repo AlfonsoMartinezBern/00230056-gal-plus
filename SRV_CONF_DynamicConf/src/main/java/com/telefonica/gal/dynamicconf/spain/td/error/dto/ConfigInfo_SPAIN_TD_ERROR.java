e5bad43f2b4cdef1d526c420a3673a14b2ac2f2c
jose maria <josemaria.gonzalez@entelgy.com>
2021-06-03 20:33:49 +0200

    info

--- SRV_CONF_DynamicConf/src/main/java/com/telefonica/gal/dynamicconf/spain/td/error/dto/ConfigInfo_SPAIN_TD_ERROR.java
+++ SRV_CONF_DynamicConf/src/main/java/com/telefonica/gal/dynamicconf/spain/td/error/dto/ConfigInfo_SPAIN_TD_ERROR.java
@@ -4,9 +4,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.telefonica.gal.dynamicconf.generic.dto.MessageResponse;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+
 
 
 @JsonInclude(Include.NON_NULL)
