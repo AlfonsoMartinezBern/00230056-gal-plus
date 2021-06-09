734e89cb5c2643de7ab12d3d3a8f37ffbe654321
Javier Sánchez González <javier.sanchez@entelgy.com>
2021-06-09 10:27:03 +0200

    Validaciones sin terminar

--- SPAIN_TD_CustomerProvision/src/main/java/com/telefonica/gal/SPAIN_TD_CustomerProvision/validator/CustomerProvisionValidator.java
+++ SPAIN_TD_CustomerProvision/src/main/java/com/telefonica/gal/SPAIN_TD_CustomerProvision/validator/CustomerProvisionValidator.java
@@ -1,9 +1,7 @@
 package com.telefonica.gal.SPAIN_TD_CustomerProvision.validator;
 
 
-import com.telefonica.gal.customerProvision.request.CUSTOMER;
-import com.telefonica.gal.customerProvision.request.CUSTOMERPROVISIONREQUEST;
-import com.telefonica.gal.customerProvision.request.TVSERVICE;
+import com.telefonica.gal.customerProvision.request.*;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Unmarshaller;
@@ -39,7 +37,7 @@ public class CustomerProvisionValidator {
             return 14;
         // Formato erróneo return 13
 
-        //SERVICE_TYPE
+        // SERVICE_TYPE
         if (customer.getSERVICETYPE().isEmpty() || customer.getSERVICETYPE().isBlank() || customer.getSERVICETYPE() == null)
             return 17;
         // Formato erróneo return 16
@@ -49,34 +47,66 @@ public class CustomerProvisionValidator {
             return 25;
         // Formato erróneo return 24
 
-        //TV_SERVICE_ID
-        for (TVSERVICE tvservice : customer.getLISTTVSERVICES().getTVSERVICE()){
+        // TV_SERVICE_ID
+        for (TVSERVICE tvservice : customer.getLISTTVSERVICES().getTVSERVICE()) {
             String serviceID = tvservice.getTVSERVICEID();
             if (serviceID.isBlank() || serviceID.isEmpty() || serviceID == null)
                 return 28;
             // Formato erróneo return 27
         }
 
-        //POP
+        // POP
         if (customer.getPOP() == 0)
             return 34;
         // Formato erróneo return 33
 
-        //SUBSCRIBER_LINE_UPSTREAM
+        // SUBSCRIBER_LINE_UPSTREAM
         if (customer.getSUBSCRIBERLINE().getUPSTREAM() == 0) return 75;
         // Formato erróneo return 76
 
-        //SUBSCRIBER_LINE_DOWNSTREAM
+        // SUBSCRIBER_LINE_DOWNSTREAM
         if (customer.getSUBSCRIBERLINE().getDOWNSTREAM() == 0) return 77;
         // Formato erróneo return 78
 
+        // VOD_SERVICE_ID, si LIST_VOD_SERVICE presente
+        for (VODSERVICE vodservice : customer.getLISTVODSERVICES().getVODSERVICE()) {
+            String vodServiceID = vodservice.getVODSERVICEID();
+            if (vodServiceID.isBlank() || vodServiceID.isEmpty() || vodServiceID == null)
+                return 31;
+            // Formato erróneo return 30
+        }
+
+        // OPERATOR_BONUS si LIST_OPERATOR_BONUS presente
+        if (customer.getLISTOPERATORBONUS().getOPERATORBONUS() != null) {
+            for (OPERATORBONUS operatorbonus : customer.getLISTOPERATORBONUS().getOPERATORBONUS()) {
+                if (operatorbonus.getValue().isEmpty() || operatorbonus.getValue().isBlank() || operatorbonus.getValue() == null || operatorbonus == null)
+                    return 31; // TODO codigo correcto
+                // Formato erróneo return 30 // TODO codigo correcto
+            }
+        }
+
+        // operation si LIST_OPERATOR_BONUS presente
+        if (customer.getLISTOPERATORBONUS().getOPERATORBONUS() != null) {
+            for (OPERATORBONUS operatorbonus : customer.getLISTOPERATORBONUS().getOPERATORBONUS()) {
+                if (operatorbonus.getOperation() == null || operatorbonus == null)
+                    return 50;
+                // Formato erróneo return 51
+            }
+        }
+
+        // TV_SERVICE_OPER si TV_SERVICE_ID presente
+        if (customer.getLISTTVSERVICES().getTVSERVICE() != null){
+            for (TVSERVICE tvservice : customer.getLISTTVSERVICES().getTVSERVICE()) {
+                if (tvservice.getTVSERVICEOPER() == null)
+                    return 28; // TODO codigo correcto
+                // Formato erróneo return 27 // TODO codigo correcto
+            }
+        }
+
+        //VOD_SERVICE_OPER si VOD_SERVICE_ID presente
+        //if (customer.getLISTVODSERVICES().getVODSERVICE())
 
         /*
-            VOD_SERVICE_ID, si LIST_VOD_SERVICE presente
-            OPERATOR_BONUS si LIST_OPERATOR_BONUS presente
-            operation si LIST_OPERATOR_BONUS presente
-            TV_SERVICE_OPER si TV_SERVICE_ID presente
-            VOD_SERVICE_OPER si VOD_SERVICE_ID presente
             ADDRESSING (solo debe comprobarse el formato)
             MAX_NUM_STBS (solo debe comprobarse el formato)
         */
@@ -90,7 +120,7 @@ public class CustomerProvisionValidator {
             Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
             CUSTOMERPROVISIONREQUEST customerprovisionrequest = (CUSTOMERPROVISIONREQUEST) jaxbUnmarshaller.unmarshal(new StringReader(request));
 
-            for(CUSTOMER customer : customerprovisionrequest.getCUSTOMERS().getCUSTOMER()){
+            for (CUSTOMER customer : customerprovisionrequest.getCUSTOMERS().getCUSTOMER()) {
                 if (!request.contains("<USER_ID>")) return 36;
                 if (!request.contains("<GEOGRAFIC_AREA>")) return 12;
                 if (!request.contains("<USER_TYPE>")) return 15;
@@ -98,14 +128,15 @@ public class CustomerProvisionValidator {
                 if (!request.contains("<TV_SERVICE_ID>")) return 29;
 
                 if (request.contains("<TV_SERVICE_ID>") && !request.contains("<VOD_SERVICE_ID>")) return 32;
-                if (request.contains("<TV_SERVICE_ID>") && countContainedString(request, "<VOD_SERVICE_ID>") != customer.getLISTTVSERVICES().getTVSERVICE().size()) return 32;
+                if (request.contains("<TV_SERVICE_ID>") && countContainedString(request, "<VOD_SERVICE_ID>") != customer.getLISTTVSERVICES().getTVSERVICE().size())
+                    return 32;
 
                 if (request.contains("<OPERATOR_BONUS>") && !request.contains("<LIST_OPERATOR_BONUS>")) return 45;
 
                 if (request.contains("<ADDRESSING>")) return 84;
             }
 
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return 0;
