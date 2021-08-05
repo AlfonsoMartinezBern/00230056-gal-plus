f6549efe8e5fad2d64f5b1f3c39b8b87ece3ac50
AlfonsoMartinezBern <alfonso.martinez.ext@entelgy.com>
2021-08-05 19:39:48 +0200

    first commit

---
+++ src/test/java/com/telefonica/gal/dynamicrouting/controller/DynamicRoutingTDControllerTest.java
@@ -0,0 +1,18 @@
+package com.telefonica.gal.dynamicrouting.controller;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@RunWith(SpringRunner.class)
+class DynamicRoutingTDControllerTest {
+
+	@Test
+	public void test() {
+		assertEquals("OK", "OK");
+	}
+	
+}
